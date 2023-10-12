fec06684c8e974bc78ecfcaa591ad4a753d2d7e2
strive3 <961844168@qq.com>
2023-10-12 21:39:29 +0800

    first commit

---
+++ src/leetcode/hash/TwoSum.java
@@ -0,0 +1,52 @@
+package leetcode.hash;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ *
+ * 你可以按任意顺序返回答案。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 示例 2：
+ *
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 示例 3：
+ *
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 7, 11, 15};
+        Arrays.stream(twoSum(arr, 9)).forEach(e -> System.out.println(e));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        // key:num[i] 数组值  value:数组下标
+        // 遍历数组，使用 target-num[i] 作为 key 来查找map中是否存在对应的值，若存在 则直接使用map.value 给结果数组赋值并返回
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            Integer integer = map.get(target - nums[i]);
+            if (integer == null) {
+                map.put(nums[i], i);
+            } else {
+                result[0] = integer;
+                result[1] = i;
+                return result;
+            }
+        }
+        return result;
+    }
+}
